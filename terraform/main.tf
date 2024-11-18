7a5b8b133bafc02691c930f9d83816f77a5c92b8
mk417 <kwakou.missoh@lalux.lu>
2024-11-18 10:44:07 +0100

    updated main.tf [skip ci]

--- terraform/main.tf
+++ terraform/main.tf
@@ -55,26 +55,28 @@ data "nutanix_cluster" "dc3_clusters" {
   provider = nutanix.dc3
 }
 
-# data "nutanix_image" "dc1_images" {
-#     for_each = local.dc1_images 
-#     image_name = each.value.name 
-#     provider = nutanix.dc1
-# }
-
-# data "nutanix_image" "dc3_images" {
-#     for_each = local.dc3_images 
-#     image_name = each.value.name  
-#     provider = nutanix.dc3
-# }
-data "nutanix_image" "rhel8-dc1" {
-  image_name = "RHEL8STD-latest"
-  provider   = nutanix.dc1
+data "nutanix_image" "dc1_images" {
+    for_each = local.dc1_images 
+    image_id = each.value.uuid
+    image_name = each.value.name
+    provider = nutanix.dc1
 }
 
-data "nutanix_image" "rhel8-dc3" {
-  image_name = "RHEL8STD-latest"
-  provider   = nutanix.dc3
+data "nutanix_image" "dc3_images" {
+    for_each = local.dc3_images 
+    image_id = each.value.uuid
+    image_name = each.value.name
+    provider = nutanix.dc3
 }
+# data "nutanix_image" "rhel8-dc1" {
+#   image_name = "RHEL8STD-latest"
+#   provider   = nutanix.dc1
+# }
+
+# data "nutanix_image" "rhel8-dc3" {
+#   image_name = "RHEL8STD-latest"
+#   provider   = nutanix.dc3
+# }
 
 
 # ============= VMWARE ESX INFRA ================
