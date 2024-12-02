f7e6031c2d51a3e09832d300b4db33fa80fe8288
mk417 <kwakou.missoh@lalux.lu>
2024-12-02 15:27:31 +0100

    [reset localadmin] modified tf generator to set the path to => keys/localadmin.pub [skip ci]

--- backend/tf_generator.py
+++ backend/tf_generator.py
@@ -18,7 +18,7 @@ def generate_lan_vm_config(vm_name, vm_specs):
     return f"""
       # Read the SSH public key
       data "local_file" "ssh_public_key" {{
-        filename = "../keys/localadmin.pub"
+        filename = "keys/localadmin.pub"
       }}
 
       resource "nutanix_virtual_machine" "{vm_name}" {{
