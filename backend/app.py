1b00e41813d379f3f80f072bad65f407a5635b69
mk417 <kwakou.missoh@lalux.lu>
2024-12-16 16:16:09 +0100

    important refactoring -- remove esx related conf [skip ci]

--- backend/app.py
+++ backend/app.py
@@ -62,28 +62,26 @@ def basic_auth():
     return credentials
 
 def get_domains():
-    return ['LAN','DMZ']
+    return ['NUTANIX']
 
 def get_environment():
     return ['DEV_TEST', 'RECETTE_PREPROD', 'PROD']
 
 def get_datacenters(domain):
-    if domain == "LAN":
-        return ['dc1','dc3']
-    else:
-        return ['NUT-DMZ-DC01','NUT-DMZ-DC02']
+    if domain == "NUTANIX":
+        return ['dc1','dc3','NUT-AHV-DMZ-DC01','NUT-AHV-DMZ-DC03']
+    # else:
+    #     return ['NUT-DMZ-DC01','NUT-DMZ-DC02']
 
 def get_ahv_pc(datacenter):
-    return {"dc1":"lu652.lalux.local","dc3":"lu653.lalux.local"}[datacenter]
+    return {"dc1":"lu652.lalux.local","dc3":"lu653.lalux.local","NUT-AHV-DMZ-DC01":"lu652.lalux.local","NUT-AHV-DMZ-DC03":"lu653.lalux.local"}[datacenter]
 
-def get_esx_vcenter():
-    return "lu309.lalux.local"
 def get_images(datacenter):
     images = {
         'dc1': ['rhel8-dc1'],
         'dc3': ['rhel8-dc3'],
-        'NUT-DMZ-DC01': ['esx_lib1_item'],
-        'NUT-DMZ-DC02': ['esx_lib2_item']        
+        'NUT-AHV-DMZ-DC01': ['rhel8-dc1'],
+        'NUT-AHV-DMZ-DC03': ['rhel8-dc3'],
     }
 
     return images.get(datacenter,[])
@@ -91,9 +89,7 @@ def get_images(datacenter):
 def get_clusters(datacenter):
     clusters = {
         'dc1': ['lu650.lalux.local'],
-        'dc3': ['lu651.lalux.local'],
-        'NUT-DMZ-DC01': ['nut-dmz-01','nut-dmz-03','nut-dmz-05','nut-dmz-09'],
-        'NUT-DMZ-DC02': ['nut-dmz-02','nut-dmz-04','nut-dmz-06','nut-dmz-10'],
+        'dc3': ['lu651.lalux.local'],        
         'NUT-AHV-DMZ-DC01': ['nut-ahv-dmz-dc1.lalux.local'],
         'NUT-AHV-DMZ-DC03': ['nut-ahv-dmz-dc3.lalux.local']
     }
@@ -129,39 +125,6 @@ def get_ahv_subnets(datacenter):
 
     return subnets_names
 
-def get_esx_subnets():
-    load_dotenv()
-    username=os.getenv('ESX_USERNAME') #!!! CHANGE CREDENTIALS TO A PRIVILEGED ESX SA !!!
-    password=os.getenv('ESX_PASSWORD')
-    # set to the first ESX host since networks in vsphere are created across all hosts (unless exception)
-    esx_host = "nut-dmz-01.lalux.local"
-    vcenter = get_esx_vcenter()
-
-    try:
-        context = ssl.create_default_context()
-        context.check_hostname = False 
-        context.verify_mode = ssl.CERT_NONE 
-        
-        si = connect.SmartConnect(
-            host=vcenter,
-            user=username,
-            pwd=password,
-            sslContext=context
-        )
-        
-    except Exception as e:
-        print(f"An error occured during the connection request: {e}")
-
-    content = si.RetrieveContent()
-    network_view = content.viewManager.CreateContainerView(content.rootFolder, [vim.Network], True)
-    networks = network_view.view
-
-    subnet_names = [network.name for network in networks if isinstance(network, vim.Network)]
-
-    connect.Disconnect(si)
-    return subnet_names 
-
-
 def get_ahv_storages(cluster):
     #couldnt get this to work with http.client -- open a support case
     load_dotenv()
@@ -177,37 +140,6 @@ def get_ahv_storages(cluster):
         print(e)
     return [ storage['name'] for storage in storage_containers]
 
-
-def get_esx_datastores():
-    load_dotenv()
-    username = os.getenv('ESX_USERNAME')
-    password = os.getenv('ESX_PASSWORD')
-    vcenter = get_esx_vcenter()
-
-    try:
-        context = ssl.create_default_context()
-        context.check_hostname = False 
-        context.verify_mode = ssl.CERT_NONE 
-        
-        si = connect.SmartConnect(
-            host=vcenter,
-            user=username,
-            pwd=password,
-            sslContext=context
-        )
-        
-        content = si.RetrieveContent()
-        datastore_view = content.viewManager.CreateContainerView(content.rootFolder, [vim.Datastore], True)
-        datastores = datastore_view.view
-
-        datastore_names = [datastore.name for datastore in datastores]
-
-        connect.Disconnect(si)
-        return datastore_names
-    
-    except Exception as e:
-        print(f"An error occurred while retrieving ESX datastores: {e}")
-        return []
 
 @app.route('/')
 def index():
@@ -231,19 +163,13 @@ def images():
 @app.route('/get_subnets')
 def subnets():
     datacenter = request.args.get('datacenter')
-    if "DMZ" in datacenter:
-      return jsonify(get_esx_subnets())
-    else:
-      return jsonify(get_ahv_subnets(datacenter))
+    return jsonify(get_ahv_subnets(datacenter))
 
 @app.route('/get_storages')
 def storages():
     cluster = request.args.get('cluster')
     print(cluster)
-    if 'dmz' in cluster:
-        return jsonify(get_esx_datastores())
-    else:
-        return jsonify(get_ahv_storages(cluster))
+    return jsonify(get_ahv_storages(cluster))
 
 
 @app.route('/submit', methods=["POST"])
--- backend/tf_generator.py
+++ backend/tf_generator.py
@@ -8,15 +8,14 @@ def generate_terraform_config(yaml_data):
     vm_data = yaml.safe_load(yaml_data)                                           
                                                                                   
     terraform_lan_config = ""                                                     
-    terraform_dmz_config = ""                                                     
                                                                                   
     for vm_name, vm_specs in vm_data.items():                                     
-        if vm_specs['domain'] == 'LAN':                                           
+        if vm_specs['domain'] == 'NUTANIX':                                           
             terraform_lan_config += generate_lan_vm_config(vm_name, vm_specs)     
-        else:                                                                     
-            terraform_dmz_config += generate_dmz_vm_config(vm_name, vm_specs)     
+        # else:                                                                     
+        #     terraform_dmz_config += generate_dmz_vm_config(vm_name, vm_specs)     
                                                                                   
-    return terraform_lan_config,terraform_dmz_config                              
+    return terraform_lan_config
 
 def generate_lan_vm_config(vm_name, vm_specs):
     return f"""
@@ -77,7 +76,7 @@ def generate_lan_vm_config(vm_name, vm_specs):
 # This function would be called after processing the form data
 def create_terraform_file(yaml_data):
     try:
-      terraform_lan_config,terraform_dmz_config = generate_terraform_config(yaml_data)
+      terraform_lan_config = generate_terraform_config(yaml_data)
       # Debug output
       print("Generated Terraform config:")
       print(terraform_lan_config)
